fd72f200b9f49917f458259b4ae2e8138b17ebff
PierreP <pinatelp@gmail.com>
2018-09-11 16:58:37 +0200

    DAO

--- src/main/java/fr/epf/demoseptembre/persistence/OfferDao.java
+++ src/main/java/fr/epf/demoseptembre/persistence/OfferDao.java
@@ -1,4 +1,15 @@
 package fr.epf.demoseptembre.persistence;
 
-public interface OfferDao {
+import fr.epf.demoseptembre.models.Offer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * TODO class details.
+ *
+ * @author Loïc Ortola on 10/09/2018
+ */
+@Repository
+public interface OfferDao extends CrudRepository<Offer, Integer> {
+
 }
